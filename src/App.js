bcc46ca80a8daae396c4057a102af5218b0e3942
Mohit Dhatrak <91209576+mohitdhatrak@users.noreply.github.com>
2021-09-25 23:39:02 +0530

    completed mark 8 project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,124 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [userInput, setUserInput] = useState("");
+
+  var emojiDictionary = {
+    "🧗": "Person Climbing",
+    "🤺": "Person Fencing",
+    "🏇": "Horse Racing",
+    "⛷️": "Skier",
+    "🏂": "Snowboarder",
+    "🏌️": "Person Golfing",
+    "🏄": "Person Surfing",
+    "🚣": "Person Rowing Boat",
+    "🏊": "Person Swimming",
+    "⛹️": "Person Bouncing Ball",
+    "🏋️": "Person Lifting Weights",
+    "🚴": "Person Biking",
+    "🚵": "Person Mountain Biking",
+    "🤸": "Person Cartwheeling",
+    "🤼": "People Wrestling",
+    "🤽": "Person Playing Water Polo",
+    "🤾": "Person Playing Handball",
+    "🤹": "Person Juggling",
+    "🧘": "Person in Lotus Position",
+    "🛹": "Skateboard",
+    "🛼": "Roller Skate",
+    "🛶": "Canoe",
+    "🎗️": "Reminder Ribbon",
+    "🎟️": "Admission Tickets",
+    "🎫": "Ticket",
+    "🎖️": "Military Medal",
+    "🏆": "Trophy",
+    "🏅": "Sports Medal",
+    "🥇": "1st Place Medal",
+    "🥈": "2nd Place Medal",
+    "🥉": "3rd Place Medal",
+    "⚽": "Soccer Ball",
+    "⚾": "Baseball",
+    "🥎": "Softball",
+    "🏀": "Basketball",
+    "🏐": "Volleyball",
+    "🏈": "American Football",
+    "🏉": "Rugby Football",
+    "🎾": "Tennis",
+    "🥏": "Flying Disc",
+    "🎳": "Bowling",
+    "🏏": "Cricket Game",
+    "🏑": "Field Hockey",
+    "🏒": "Ice Hockey",
+    "🥍": "Lacrosse",
+    "🏓": "Ping Pong",
+    "🏸": "Badminton",
+    "🥊": "Boxing Glove",
+    "🥋": "Martial Arts Uniform",
+    "🥅": "Goal Net",
+    "⛳": "Flag in Hole",
+    "⛸️": "Ice Skate",
+    "🎣": "Fishing Pole",
+    "🎽": "Running Shirt",
+    "🎿": "Skis",
+    "🛷": "Sled",
+    "🥌": "Curling Stone",
+    "🎯": "Direct Hit",
+    "🎱": "Pool 8 Ball",
+    "🎮": "Video Game",
+    "🎰": "Slot Machine",
+    "🎲": "Game Die",
+    "🧩": "Puzzle Piece",
+    "♟️": "Chess Pawn",
+    "🏹": "Bow and Arrow"
+  };
+
+  function inputText(event) {
+    var userInput = event.target.value;
+    if (userInput === "") {
+      setUserInput("");
+    } else {
+      var emoji = emojiDictionary[userInput];
+      if (emoji === undefined) {
+        setUserInput(
+          "Sorry, this emoji is not available in our database, please try a sport-related emoji"
+        );
+      } else {
+        setUserInput(emoji);
+      }
+      console.log(emoji);
+      console.log(userInput);
+    }
+  }
+
+  var emojiArray = Object.keys(emojiDictionary);
+
+  function clickHandler(emojidata) {
+    var meaning = emojiDictionary[emojidata];
+    setUserInput(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="header">Sports Emoji's Meaning</h1>
+      <div className="output-text">{userInput}</div>
+      <input
+        className="input-text"
+        onChange={inputText}
+        placeholder="Type your emoji here"
+      ></input>
+      <h3>Or choose one emoji to know it's meaning</h3>
+      <ul className="emoji-container">
+        {emojiArray.map((emojiSymbol) => (
+          <li
+            className="emoji-style"
+            onClick={function () {
+              clickHandler(emojiSymbol);
+            }}
+          >
+            {emojiSymbol}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
